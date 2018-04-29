385a35bbd15163cc5bb5c5d9ad245b08b3e6e860
Airubby <ai_rubby@qq.com>
2018-04-29 23:24:13 +0800

    内容页

--- telweb/js/basic.js
+++ telweb/js/basic.js
@@ -1,18 +1,29 @@
- function GetRequest() {
-    var url = location.search; //获取url中"?"符后的字串
-    var theRequest = new Object();
-    if (url.indexOf("?") != -1) {
-        var str = url.substr(1);
-        strs = str.split("&");
-        for(var i = 0; i < strs.length; i ++) {
-            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-        }
-    }
-    console.log(theRequest)
-    return theRequest;
-}
-console.log(GetRequest())
 
 jQuery(".index_slideBox").slide({mainCell:".bd ul",effect:"fold",autoPlay:true});
 
 jQuery(".slideTabBox").slide({delayTime:0});
+
+// 内容页
+function anchorGoWhere(nav,yangshi,sudu,toubu){
+    a=nav+" a";
+    $(a).click(function(){
+        tis=$(this);
+        o=tis.attr("href");
+        $("html,body").animate({scrollTop: $(o).offset().top-toubu}, sudu);
+        return false;
+    });
+    $(window).scroll(function() {
+        t = $(this).scrollTop();
+        $(a).each(function(){
+            d=$(this).attr("href");
+            y = $(d).offset().top;
+            jl=t-y;
+            $(this).parent().removeClass(yangshi);
+            if (-81<jl && jl<$(d).height()-40){
+                $(this).parent().addClass(yangshi);
+            }
+        });
+    });
+}
+anchorGoWhere("#show_nav","active",500,40);
+$("#show_nav").scrollFix({distanceTop:0});
